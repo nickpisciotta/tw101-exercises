5808a7f86afaeac9e32e11d387faa644806f40c1
Nick Pisciotta <npisciotta@gmail.com>
2017-03-14 13:25:38 -0600

    Finish exercise 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,7 +5,17 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        int total = 0;
+        for (int i = 1; i < 100; i++) {
+            if (isOdd(i)) {
+                System.out.println(i);
+                total = total + i;
+            }
+        }
+        System.out.println("Total of all odd numbers: " + total);
+    }
 
+    public static boolean isOdd (double number) {
+          return  (number % 2 == 1) ? true : false;
     }
 }
