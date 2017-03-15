0235c8e131193089b7b090685dbc4f935c2ad61a
Nick Pisciotta <npisciotta@gmail.com>
2017-03-15 13:55:30 -0600

    Finish exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,16 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int totalOfOdds = 0;
+        for (int i = start + 1; i < end; i++) {
+            if (isOdd(i)) {
+                totalOfOdds = totalOfOdds + i;
+            }
+        }
+        return totalOfOdds;
+    }
+
+    public boolean isOdd (double number) {
+        return  (number % 2 == 1) ? true : false;
     }
 }
